5d3d6d02d042cfee66c75b906a0df58acc4f73ff
ImZacvin <cdarmajaya@gmail.com>
2025-06-16 14:41:11 +0700

    Create oop version of cart

--- exercise/javascript-amazon-project/data/cart.js
+++ exercise/javascript-amazon-project/data/cart.js
@@ -1,17 +1,22 @@
-export let cart = JSON.parse(localStorage.getItem('cart'));
+export let cart;
 
-if(!cart) {
-  cart = [{
-    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity: 2,
-    deliveryOptionId: '1'
-  }, {
-    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 1,
-    deliveryOptionId: '2'
-  }]
-};
+loadFromStorage();
 
+export function loadFromStorage() {
+  cart = JSON.parse(localStorage.getItem('cart'));
+
+  if (!cart) {
+    cart = [{
+      productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+      quantity: 2,
+      deliveryOptionId: '1'
+    }, {
+      productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+      quantity: 1,
+      deliveryOptionId: '2'
+    }];
+  }
+}
 
 function saveToStorage() {
   localStorage.setItem('cart', JSON.stringify(cart));
@@ -21,16 +26,16 @@ export function addToCart(productId) {
   let matchingItem;
 
   cart.forEach((cartItem) => {
-    if(productId === cartItem.productId) {
+    if (productId === cartItem.productId) {
       matchingItem = cartItem;
     }
   });
 
-  if(matchingItem) {
-    matchingItem.quantity++;
+  if (matchingItem) {
+    matchingItem.quantity += 1;
   } else {
     cart.push({
-      productId,
+      productId: productId,
       quantity: 1,
       deliveryOptionId: '1'
     });
@@ -41,8 +46,9 @@ export function addToCart(productId) {
 
 export function removeFromCart(productId) {
   const newCart = [];
+
   cart.forEach((cartItem) => {
-    if(cartItem.productId !== productId) {
+    if (cartItem.productId !== productId) {
       newCart.push(cartItem);
     }
   });
@@ -56,7 +62,7 @@ export function updateDeliveryOption(productId, deliveryOptionId) {
   let matchingItem;
 
   cart.forEach((cartItem) => {
-    if(productId === cartItem.productId) {
+    if (productId === cartItem.productId) {
       matchingItem = cartItem;
     }
   });
